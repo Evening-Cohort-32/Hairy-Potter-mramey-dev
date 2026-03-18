8781f1b661f689d6671e6fd5e986d0efa6e9398b
Micah Ramey <micahtaylor17@gmail.com>
2026-03-17 20:49:47 -0500

    initial commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,16 @@
 // Imports go first
-
-
+import { makePottery } from "./PotteryWheel.js";
 
 // Make 5 pieces of pottery at the wheel
-let mug = makePottery()
+let mug = makePottery("Mug", 2, 4);
+let bowl = makePottery("Bowl", 3, 3);
+let plate = makePottery("Plate", 3, 1);
+let teapot = makePottery("Teapot", 4, 5);
+let vase = makePottery("Vase", 5, 6);
 
-
+console.log(mug, bowl, plate, teapot, vase);
 // Fire each piece of pottery in the kiln
-
 
 // Determine which ones should be sold, and their price
 
-
 // Invoke the component function that renders the HTML list
-
